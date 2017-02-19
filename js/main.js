8496aee60e8e3b1be9ee71a62117d334f3236e12
codefine <you@277133779@qq.com>
2017-02-19 17:35:18 +0800

    change
    change

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(function () {
 				iStatus++;
 				oSpan.html(Math.floor(iStatus/imgData.length*100));
 				if (iStatus==imgData.length) {
+					oSpan.parent().fadeOut();
 					oPreload.slideUp(1600,function (){
 						$(this).remove();
 					});
