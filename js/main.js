af04b376453164bb6a58fd8b8de12b7c85bbceb5
codefine <you@277133779@qq.com>
2017-02-19 17:48:05 +0800

    change
    change

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-$(function () {
-	//预加载
-	var oPreload = $('.preload');
-		var oSpan = $('.preload span');
-		var iStatus = 0;
-		for (var i=0; i<imgData.length; i++) {
-			var oImg = new Image();
-			oImg.onload = function (){
-				iStatus++;
-				oSpan.html(Math.floor(iStatus/imgData.length*100));
-				if (iStatus==imgData.length) {
-					oSpan.parent().fadeOut();
-					oPreload.slideUp(1600,function (){
-						$(this).remove();
-					});
-				}
+//预加载
+var oPreload = $('.preload');
+	var oSpan = $('.preload span');
+	var iStatus = 0;
+	for (var i=0; i<imgData.length; i++) {
+		var oImg = new Image();
+		oImg.onload = function (){
+			iStatus++;
+			oSpan.html(Math.floor(iStatus/imgData.length*100));
+			if (iStatus==imgData.length) {
+				oSpan.parent().fadeOut();
+				oPreload.slideUp(1600,function (){
+					$(this).remove();
+				});
 			}
-			oImg.src = imgData[i];
 		}
-	
-	//bootstrap小提示
-	$('[data-toggle="tooltip"]').tooltip();
-});
+		oImg.src = imgData[i];
+	}
+
+//bootstrap小提示
+$('[data-toggle="tooltip"]').tooltip();
